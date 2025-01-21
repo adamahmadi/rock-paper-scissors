bf530af29b6c039df0906ea55bde070d8d3df226
adamahmadi <adam_ahmadi@hotmail.com>
2025-01-21 18:58:41 +0800

    Tried creating getComputerChoice function - not working

--- rps.js
+++ rps.js
@@ -1,1 +1,13 @@
-console.log ("Hello World! This is a test to see if my js file is properly linked")
+/* Writing the first function to return one of the options randomly */
+function getComputerChoice () {
+    return Math.random ();
+    if (getComputerChoice <= 0.34) {
+        return "Rock!";
+    } else if (getComputerChoice <= 0.67 && getComputerChoice >= 0.35) {
+        return "Paper!";
+    } if (getComputerChoice >= 0.68) {
+        return "Scissors!";
+    }
+    }
+
+console.log (Math.random)
