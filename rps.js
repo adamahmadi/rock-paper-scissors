8ce6c8e177bc4c7b73e1e38b92156cd583223652
adamahmadi <adam_ahmadi@hotmail.com>
2025-01-22 22:01:28 +0800

    fixed getComputerChoice and tested function

--- rps.js
+++ rps.js
@@ -1,13 +1,17 @@
 /* Writing the first function to return one of the options randomly */
 function getComputerChoice () {
-    return Math.random ();
-    if (getComputerChoice <= 0.34) {
+    /* Generating a random number between 0 and 2 */
+    const randomNumber = Math.floor (Math.random() * 3);
+
+    /* Return corresponding choice based on the random number */
+    if (randomNumber === 0) {
         return "Rock!";
-    } else if (getComputerChoice <= 0.67 && getComputerChoice >= 0.35) {
+    } else if (randomNumber === 1) {
         return "Paper!";
-    } if (getComputerChoice >= 0.68) {
+    } if (randomNumber === 2) {
         return "Scissors!";
     }
     }
 
-console.log (Math.random)
+    /* Testing function for functionality */
+console.log (getComputerChoice())
